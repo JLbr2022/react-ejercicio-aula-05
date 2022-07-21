62888e47c0a5b70fe2c5c3f499e7922f7e93e6c9
José Freites <jlfu2004@gmail.com>
2022-07-21 14:09:02 -0300

    First Commit

--- src/components/MyInfo/MyInfo.js
+++ src/components/MyInfo/MyInfo.js
@@ -1,20 +1,6 @@
-// ? Prática
-// ● Vamos explorar Componentes Funcionais:
-// ○ Crie um projeto React do zero (dica: create-react-app)
-// ○ Crie um Componente Funcional com o nome MyInfo
-// ○ Adicione um elemento de título com seu nome (dica: <h1>)
-// ○ Adicione um elemento de parágrafo com uma breve descrição sobre você (dica: <p>)
-// ○ Crie uma lista ordenada com três coisas que vocês gosta de fazer nas suas horas livres
-// ○ Renderize seu Componente Funcional no navegador
-// ○ [EXTRA] Ainda não falamos sobre estilos.... mas que tal pesquisar sobre como isto acontece
-// no React e fazer uma tentativa?
-
 import React from "react";
-import "./App.css";
-import MyInfo from "./components/MyInfo/MyInfo.js";
-import FlamingoSelfie from "./images/FlamingoSelfie.jpg";
 
-function App() {
+function MyInfo(props) {
   return (
     <div className="App">
       <header className="header centered">
@@ -27,7 +13,7 @@ function App() {
         <div className="MyPhoto">
           <img
             className="imgMyPhoto"
-            src={FlamingoSelfie}
+            src={props.FlamingoSelfie}
             alt="FlamingoSelfie"
           />
         </div>
@@ -63,5 +49,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
