c56fdd7e98722cc33024920ea917ca44546cb2fe
José Freites <jlfu2004@gmail.com>
2022-07-21 20:01:11 -0300

    ADD: responsivity

--- src/components/MyInfo/MyInfo.js
+++ src/components/MyInfo/MyInfo.js
@@ -2,34 +2,36 @@ import React from "react";
 import "./MyInfo.css";
 function MyInfo(props) {
   return (
-    <div className="App">
+    <div className="container">
       <header className="header centered">
         <div className="Title">
           <h1>Hola!, soy José Luis Freites</h1>
         </div>
       </header>
 
-      <main className="main centered">
-        <div className="MyPhoto">
-          <img
-            className="imgMyPhoto"
-            src={require(`../images/${props.photo}.jpg`)}
-            alt="MyPhoto"
-          />
-        </div>
+      <div className="App">
+        <main className="main centered">
+          <div className="MyPhoto">
+            <img
+              className="imgMyPhoto"
+              src={require(`../images/${props.photo}.jpg`)}
+              alt="MyPhoto"
+            />
+          </div>
 
-        <div className="Paragraph">
-          <p className="p-content">{props.description}</p>
-        </div>
-        <div className="List">
-          <h3>En mi tiempo libre me gusta:</h3>
-          <ol className="freeTime">
-            <li className="List01">{props.List01}</li>
-            <li className="List02">{props.List02}</li>
-            <li className="List03">{props.List03}</li>
-          </ol>
-        </div>
-      </main>
+          <div className="Paragraph">
+            <p className="p-content">{props.description}</p>
+          </div>
+          <div className="List">
+            <h3>En mi tiempo libre me gusta:</h3>
+            <ol className="freeTime">
+              <li className="List01">{props.List01}</li>
+              <li className="List02">{props.List02}</li>
+              <li className="List03">{props.List03}</li>
+            </ol>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
